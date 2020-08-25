195758a310e23e98ac3c9fc32fbeb7ce813f9254
SapnaNattuvetty <sapna.nattuvetty@gmail.com>
2020-08-25 15:42:28 +0000

    using format string to print

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-print('Hello World!')
 name = input("Enter your name:")
-print("Hello, " + name + "!")
+print(f"Hello, " + name + "!")
 age = input("Enter your age")
 age = int (age)
-print("you will be " + str(age + 1) + " next year")
+print(f"You will be " + str(age + 1) + " next year")
