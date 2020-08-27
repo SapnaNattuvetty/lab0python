d21e68102d36226ff40f9805c0051a7c19f595b3
SapnaNattuvetty <sapna.nattuvetty@gmail.com>
2020-08-27 18:16:05 +0000

    completed the requirements for python assignment

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+print("hello world")
 name = input("Enter your name:")
-print(f"Hello, " + name + "!")
-age = input("Enter your age")
-age = int (age)
-print(f"You will be " + str(age + 1) + " next year")
+
+print("Hello, " + name + "!")
+age = input("Enter your age: ")
+age = int(age)
+print(age + 1)
+
+print("You will be " + str(age + 1) + " next year")
+
+print(f"i am {age + 1} next year")
