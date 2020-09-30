539afcf0ac1321e43612f7894f0f1696dc6e3bf0
SapnaNattuvetty <sapna.nattuvetty@gmail.com>
2020-09-30 21:53:33 +0000

    changed wording

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 
 name = input("Enter your name: ")
 
-print("Hello, " + name + "!\n")
+print("Hello, " + name + "!")
 age = input("Enter your age: ")
 age = int(age)
-print(age + 1)
-
-print("You will be " + str(age + 1) + " next year\n")
-print(f"i am {age + 1} next year")
+print("You will be " + str(age + 1) + " next year")
+# print(f"i am {age + 1} next year")
