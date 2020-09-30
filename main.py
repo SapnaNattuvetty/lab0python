35610d4eb47f6a9e47281272e15d6846f20d07e1
SapnaNattuvetty <sapna.nattuvetty@gmail.com>
2020-09-30 22:16:10 +0000

    print f

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-
+print("hello world")
 name = input("Enter your name: ")
-
-print("Hello, " + name + "!")
+print(f"Hello, {name}!")
 age = input("Enter your age: ")
 age = int(age)
-print("You will be " + str(age + 1) + " next year")
+print(f"You will be {age + 1} next year")
 # print(f"i am {age + 1} next year")
